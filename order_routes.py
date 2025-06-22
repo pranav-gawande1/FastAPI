ba1ed7b6b1451ceb4e42c43ec677dbed0ef0d769
Pranav Gawande <pjgawande1966@gmail.com>
2025-06-22 17:07:37 +0530

    Initial Commit

---
+++ order_routes.py
@@ -0,0 +1,87 @@
+from fastapi import APIRouter,status,Depends
+from fastapi_jwt_auth import AuthJWT
+from models import User,Order
+from fastapi.exceptions import HTTPException
+from database import Session,engine
+from fastapi.encoders import jsonable_encoder
+from schemas import OrderModel
+
+order_router = APIRouter(
+    prefix='/orders',
+    tags=['orders']
+)
+
+session=Session(bind=engine)
+
+@order_router.get('/')
+async def hello(Authorize:AuthJWT=Depends()):
+
+    try:
+        Authorize.jwt_required()
+
+    except Exception as e:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Invalid Token"
+        )
+    return {"message": "Multiplication of 2 and 4 is 8"}
+
+@order_router.post('/order',status_code=status.HTTP_201_CREATED)
+async def place_an_order(order:OrderModel,Authorize:AuthJWT=Depends()):
+    try:
+        Authorize.jwt_required()
+
+    except Exception as e:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Invalid Token"
+        )
+    
+    current_user=Authorize.get_jwt_subject()
+
+
+    user=session.query(User).filter(User.username==current_user).first()
+
+
+    new_order=Order(
+        pizza_size=order.pizza_size,
+        quantity=order.quantity
+    )
+    
+    new_order.user=user
+
+    session.add(new_order)
+
+    session.commit()
+
+    response={
+        "pizza_size":new_order.pizza_size,
+        "quantity":new_order.quantity,
+        "id":new_order.id,
+        "order_status":new_order.order_status
+    }
+    return jsonable_encoder(response)
+
+
+
+@order_router.get('/orders')
+async def list_all_orders(Authorize:AuthJWT=Depends()):
+    try:
+        Authorize.jwt_required()
+    except Exception as e:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED,
+                            detail="Invalid Token"
+                            )
+    
+    current_user=Authorize.get_jwt_subject()
+
+    user=session.query(User).filter(User.username==current_user)
+
+    if user.is_staff:
+        orders=session.query(Order).all()
+
+        return jsonable_encoder(orders)
+    
+    raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED,
+                            detail="You are not a Super User"
+                            )
---
+++ schemas.py
@@ -0,0 +1,50 @@
+from pydantic import BaseModel
+from sqlalchemy.orm import relationship
+from typing import Optional
+
+
+class SignUpModel(BaseModel):
+    id:Optional[int]
+    username:str
+    email:str
+    password:str
+    is_staff:Optional[bool]
+    is_active:Optional[bool]
+
+
+    class Config:
+        orm_mode=True
+        schema_extra={
+            'example':{
+                "username":"pranbot",
+                "email":"pjgawande1966@gmail.com",
+                "password":"password",
+                "is_staff":False,
+                "is_active":True
+            }
+        }
+
+
+class Settings(BaseModel):
+    authjwt_secret_key:str='879c4f426b7065b2b4ded920b9582a4fef98458f35a73023594ba94e031fdc7f'
+
+
+class LoginModel(BaseModel):
+    username:str
+    password:str
+
+class OrderModel(BaseModel):
+    id:Optional[int]
+    quantity:int
+    order_status:Optional[str]="PENDING"
+    pizza_size:Optional[str]="SMALL"
+    user_id:Optional[int]
+
+    class Config:
+        orm_mode=True
+        schema_extra={
+            'example':{
+                "quantity":2,
+                "pizza_size":"LARGE"
+            }
+        }
