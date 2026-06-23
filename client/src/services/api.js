d7013e9e78e77d80f05ecbc360c975742c74792f
pranav-gawande1 <pjgawande1966@gmail.com>
2026-06-23 19:58:15 +0530

    feat: added backend url

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,4 +1,4 @@
-const BASE_URL = import.meta.env.BACKEND_URL || "http://localhost:3000";
+const BASE_URL = import.meta.env.BACKEND_URL || "https://pizza-paradise-server.onrender.com";
 
 export const apiRequest = async ({
     endpoint,
